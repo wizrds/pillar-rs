f2f6701506b55c82553dfeb40b2301619384dee6
Timothy Pogue <poguectimothy@gmail.com>
2026-05-06 18:06:16 -0600

    fix: Improve generated code for models to provide more intuitive usage

--- crates/pillar-macros/src/model.rs
+++ crates/pillar-macros/src/model.rs
@@ -1,10 +1,9 @@
 use darling::FromDeriveInput;
 use proc_macro2::TokenStream;
-use quote::quote;
 use syn::DeriveInput;
 
 use crate::attr::ModelAttrs;
-use crate::codegen::{columns_body, resolve_name};
+use crate::codegen::{companion_module, resolve_name};
 
 
 pub fn derive(input: DeriveInput) -> TokenStream {
@@ -16,41 +15,8 @@ pub fn derive(input: DeriveInput) -> TokenStream {
 
 fn impl_model(attrs: ModelAttrs) -> syn::Result<TokenStream> {
     let ident = &attrs.ident;
-    let (impl_generics, ty_generics, where_clause) = attrs.generics.split_for_impl();
-    let table_name = resolve_name(&attrs.ident, attrs.table.as_deref());
-    let columns_body = columns_body(&attrs.data.take_struct().unwrap().fields)?;
+    let table_name = resolve_name(ident, attrs.table.as_deref());
+    let fields = attrs.data.take_struct().unwrap().fields;
 
-    Ok(quote! {
-        impl #impl_generics ::pillar::model::Model for #ident #ty_generics #where_clause {
-            fn table_name() -> &'static str {
-                #table_name
-            }
-
-            fn columns() -> &'static [::pillar::column::ColumnDef] {
-                #columns_body
-            }
-
-            fn from_record_batch(
-                batch: ::pillar::__private::arrow::record_batch::RecordBatch,
-            ) -> ::std::result::Result<::std::vec::Vec<Self>, ::pillar::errors::Error> {
-                ::pillar::__private::serde_arrow::from_record_batch(&batch)
-                    .map_err(|e| ::pillar::errors::Error::serialization(e.to_string()))
-            }
-
-            fn to_record_batch(
-                rows: &[Self],
-            ) -> ::std::result::Result<::pillar::__private::arrow::record_batch::RecordBatch, ::pillar::errors::Error> {
-                ::pillar::__private::serde_arrow::to_record_batch(
-                    &<::std::vec::Vec<::pillar::__private::arrow::datatypes::FieldRef>
-                        as ::pillar::__private::serde_arrow::schema::SchemaLike>::from_samples(
-                        &rows.iter().collect::<::std::vec::Vec<_>>(),
-                        ::pillar::__private::serde_arrow::schema::TracingOptions::default(),
-                    )
-                    .map_err(|e| ::pillar::errors::Error::serialization(e.to_string()))?,
-                    &rows.iter().collect::<::std::vec::Vec<_>>(),
-                )
-                .map_err(|e| ::pillar::errors::Error::serialization(e.to_string()))
-            }
-        }
-    })
+    companion_module(ident, &table_name, &fields)
 }
